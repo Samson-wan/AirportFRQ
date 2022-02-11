693824331e4d728dcf229ca10d8ad9dc3a869c06
BT_1E10_11 <sample@bths.edu>
2022-02-11 10:45:32 -0500

    Done

--- src/Flight.java
+++ src/Flight.java
@@ -12,24 +12,21 @@ public class Flight
     }
 
     /** Returns the number of passengers on the flight */
-    public int getNumPassengers()
-    {
+    public int getNumPassengers(){
         return numPassengers;
     }
 
     /** Returns the price of a seat on the flight. All seats on a flight have the same price.
      *  Postcondition: The value returned is greater than or equal to 0.0.
      */
-    public double getPrice()
-    {
+    public double getPrice(){
         return price;
     }
 
     /** Returns the capacity of the flight
      *  (the maximum number of passengers the flight can carry)
      */
-    public int getCapacity()
-    {
+    public int getCapacity() {
         return capacity;
     }
 }
